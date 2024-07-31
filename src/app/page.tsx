8211e8942bf5166fe12e41538186a91f57dbcb33
hassaniqbal23 <hassaniqbalkiu@gmail.com>
2024-07-31 14:59:07 +0500

    create hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Hero from "../../components/Hero";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h2>home page</h2>
+    <main className="relative bg-black-100 flex  justify-center items-center flex-col sm:px-10 px-5 mx-auto overflow-hidden text-white">
+      <div className="max-w-7xl w-full">
+        <Hero />
       </div>
     </main>
   );
